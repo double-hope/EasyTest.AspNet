3248bf0c0b57818fe4d01b578504a43822c454f5
double-hope <nadia.prohorchuk@gmail.com>
2023-10-13 20:40:30 +0300

    add DbInitializer

---
+++ EasyTest.DAL/EasyTest.DAL/DbInitializer/DbInitializer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EasyTest.DAL.DbInitializer
+{
+	public class DbInitializer : IDbInitializer
+	{
+		private readonly ApplicationDbContext _context;
+
+		public DbInitializer(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public void Initialize()
+		{
+			try
+			{
+				if (_context.Database.GetPendingMigrationsAsync().GetAwaiter().GetResult().Count() > 0)
+				{
+					_context.Database.Migrate();
+				}
+			}
+			catch (Exception e) { }
+
+			return;
+		}
+	}
+}
