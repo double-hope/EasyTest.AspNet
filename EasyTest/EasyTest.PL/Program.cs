2a9b2a262694c595ba2628ac7d739c712d03dfe2
double-hope <nadia.prohorchuk@gmail.com>
2023-11-11 19:33:25 +0200

    inject services

--- EasyTest/EasyTest.PL/Program.cs
+++ EasyTest/EasyTest.PL/Program.cs
@@ -1,7 +1,13 @@
+using EasyTest.DAL.DbInitializer;
+using EasyTest.DAL.Repository.IRepository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+builder.Services.AddScoped<IUnitOfWork, IUnitOfWork>();
 
 var app = builder.Build();
 
@@ -19,9 +25,18 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
+SeedDatabase();
 app.MapControllerRoute(
 	name: "default",
 	pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+void SeedDatabase()
+{
+	using (var scope = app.Services.CreateScope())
+	{
+		var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+		dbInitializer.Initialize();
+	}
+}
