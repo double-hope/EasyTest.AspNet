731a3d15080c89afab8ee09a4563eb7c529def27
double-hope <nadia.prohorchuk@gmail.com>
2023-11-11 18:53:32 +0200

    implement UnitOfWork

--- EasyTest.DAL/EasyTest.DAL/Repository/IRepository/IUnitOfWork.cs
+++ EasyTest.DAL/EasyTest.DAL/Repository/IRepository/IUnitOfWork.cs
@@ -2,5 +2,14 @@
 {
 	public interface IUnitOfWork
 	{
+		IAnswerRepository Answer { get; }
+		IQuestionRepository Question { get; }
+		IQuestionTestRepository QuestionTest { get; }
+		ISessionAnswerRepository SessionAnswer { get; }
+		ISessionQuestionRepository SessionQuestion { get; }
+		ITestRepository Test { get; }
+		ITestSessionRepository TestSession { get; }
+		IUserRepository User { get; }
+		void Save();
 	}
 }
