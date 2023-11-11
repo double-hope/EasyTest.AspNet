281d640ccd867c2f445c82d82cde40b057fa6121
double-hope <nadia.prohorchuk@gmail.com>
2023-11-11 20:17:49 +0200

    add dependency to shared project

---
+++ EasyTest/EasyTest.BLL/Services/UserService.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using EasyTest.BLL.Interfaces;
+using EasyTest.DAL;
+using EasyTest.Shared.DTO.User;
+
+namespace EasyTest.BLL.Services
+{
+	public class UserService : Service, IUserService
+	{
+		public UserService(ApplicationDbContext context, IMapper mapper) : base(context, mapper)
+		{
+			
+		}
+
+		public Task<UserDto> RegisterUser(UserRegisterDto userDto)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
