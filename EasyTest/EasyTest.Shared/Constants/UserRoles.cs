d8ea1f212711a8e4877606ca6805a5ede6273b52
double-hope <nadia.prohorchuk@gmail.com>
2023-11-22 15:59:30 +0200

    add user roles and Response dto

---
+++ EasyTest/EasyTest.Shared/Constants/UserRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace EasyTest.Shared.Constants
+{
+    public static class UserRoles
+    {
+        public const string AdminRole = "admin";
+        public const string TeacherRole = "teacher";
+        public const string StudentRole = "student";
+    }
+}
