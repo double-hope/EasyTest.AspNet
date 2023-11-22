c1d37a3fe4eeb64b4063c67f8e01e9477e5f8faa
double-hope <nadia.prohorchuk@gmail.com>
2023-11-22 16:37:13 +0200

    add controller to login

---
+++ EasyTest/EasyTest.WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using EasyTest.BLL.Interfaces;
+using EasyTest.Shared.DTO.Response;
+using EasyTest.Shared.DTO.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyTest.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService) 
+        {
+            _authService = authService;
+        }
+        [HttpPost]
+        public async Task<ActionResult> LoginUser([FromBody]UserLoginDto userDto)
+        {
+            var response = await _authService.Login(userDto);
+
+            if(response.Status == Status.Success)
+            {
+                return Ok(response);
+            }
+
+            return BadRequest(response);
+        }
+    }
+}
