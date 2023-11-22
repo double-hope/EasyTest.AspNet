51be90f4d56d04e504e82479a7a3a29d5b652e6e
double-hope <nadia.prohorchuk@gmail.com>
2023-11-22 12:09:35 +0200

    add scaffolded item for identity

--- EasyTest/EasyTest.DAL/ApplicationDbContext.cs
+++ EasyTest/EasyTest.DAL/ApplicationDbContext.cs
@@ -17,5 +17,9 @@ namespace EasyTest.DAL
 		public DbSet<Test> Tests {  get; set; }
 		public DbSet<TestSession> TestSessions {  get; set; }
 		public DbSet<User> Users {  get; set; }
-	}
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
 }
