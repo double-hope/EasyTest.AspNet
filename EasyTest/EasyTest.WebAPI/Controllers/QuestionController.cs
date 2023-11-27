2e8cc6c87c17c46ffd06f72a8059ea10017241aa
double-hope <nadia.prohorchuk@gmail.com>
2023-11-27 16:41:20 +0200

    fix controller

--- EasyTest/EasyTest.WebAPI/Controllers/QuestionController.cs
+++ EasyTest/EasyTest.WebAPI/Controllers/QuestionController.cs
@@ -18,19 +18,19 @@ namespace EasyTest.WebAPI.Controllers
 			_questionService = questionService;
 		}
 
-        [HttpPost("testId/{id}")]
-        [ProducesResponseType(typeof(Response<QuestionResponseDto>), (int)HttpStatusCode.OK)]
-        public async Task<ActionResult> CreateTest(Guid id, [FromBody] QuestionDto questionDto)
-        {
-            var response = await _questionService.Create(questionDto, id);
+        //[HttpPost("testId/{id}")]
+        //[ProducesResponseType(typeof(Response<QuestionResponseDto>), (int)HttpStatusCode.OK)]
+        //public async Task<ActionResult> CreateTest(Guid id, [FromBody] QuestionDto questionDto)
+        //{
+        //    var response = await _questionService.Create(questionDto, id);
 
-            if (response.Status == ResponseStatusCodesConst.Success)
-            {
-                return Ok(response);
-            }
+        //    if (response.Status == ResponseStatusCodesConst.Success)
+        //    {
+        //        return Ok(response);
+        //    }
 
-            return BadRequest(response);
-        }
+        //    return BadRequest(response);
+        //}
         [HttpPost("testId/{id}")]
         [ProducesResponseType(typeof(Response<IEnumerable<QuestionResponseDto>>), (int)HttpStatusCode.OK)]
         public async Task<ActionResult> CreateTests(Guid id, [FromBody]IEnumerable<QuestionDto> questionsDto)
