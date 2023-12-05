c6ddcd76203a8cadc866bb8dbfdcfc713df0b0ce
double-hope <nadia.prohorchuk@gmail.com>
2023-12-05 11:38:23 +0100

    add field to store number of questions by one attempt

---
+++ EasyTest/EasyTest.DAL/Migrations/20231205103726_Add_QuestionsAttempted_To_Test.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyTest.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_QuestionsAttempted_To_Test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "QuestionsAttempted",
+                table: "Tests",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QuestionsAttempted",
+                table: "Tests");
+        }
+    }
+}
