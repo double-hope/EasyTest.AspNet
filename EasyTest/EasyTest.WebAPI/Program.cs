5f6fa6dd42be669439aff0894dc4d6286635d3d1
Nadiia Prokhorchuk <napro@smart-it.com>
2024-05-09 23:00:59 +0300

    split application run into Program ans Startup

--- EasyTest/EasyTest.WebAPI/Program.cs
+++ EasyTest/EasyTest.WebAPI/Program.cs
@@ -1,55 +1,20 @@
-using EasyTest.DAL.DbInitializer;
-using EasyTest.WebAPI.Extensions;
-using System.Text.Json.Serialization;
+using EasyTest.WebAPI;
 
 public class Program
 {
-	private static void Main(string[] args)
-	{
-		var builder = WebApplication.CreateBuilder(args);
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
 
-		builder.Services.AddControllers().AddJsonOptions(options =>
-			{
-				options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-			});
-		builder.Services.AddEndpointsApiExplorer();
-		builder.Services.AddSwaggerGen(c =>
-			{
-				c.SchemaFilter<EnumSchemaFilter>();
-			});
-
-		builder.Services.RegisterDatabase(builder.Configuration);
-		builder.Services.RegisterIdentity(builder.Configuration);
-		builder.Services.RegisterServices(builder.Configuration);
-		builder.Services.RegisterCustomServices(builder.Configuration);
-		builder.Services.AddAuthorization();
-
-		builder.Services.AddJwtAuthentication(builder.Configuration);
-
-		var app = builder.Build();
-
-		if (app.Environment.IsDevelopment())
-		{
-			app.UseSwagger();
-			app.UseSwaggerUI();
-		}
-
-		app.UseHttpsRedirection();
-
-		app.UseAuthentication();
-		app.UseAuthorization();
-
-		app.MapControllers();
-		SeedDatabase();
-		app.Run();
-
-		void SeedDatabase()
-		{
-			using (var scope = app.Services.CreateScope())
-			{
-				var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
-				dbInitializer.Initialize();
-			}
-		}
-	}
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                config.Build();
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
 }
---
+++ EasyTest/EasyTest.WebAPI/Startup.cs
@@ -0,0 +1,87 @@
+﻿
+using System.Text.Json.Serialization;
+
+using EasyTest.DAL.DbInitializer;
+using EasyTest.WebAPI.Extensions;
+
+namespace EasyTest.WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+        }
+
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            });
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                c.SchemaFilter<EnumSchemaFilter>();
+            });
+
+            services.RegisterDatabase(Configuration);
+            services.RegisterIdentity(Configuration);
+            services.RegisterServices(Configuration);
+            services.RegisterCustomServices(Configuration);
+            services.AddAuthorization();
+
+            services.AddJwtAuthentication(Configuration);
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder =>
+                    {
+                        builder
+                           .AllowAnyMethod()
+                           .AllowAnyHeader()
+                           .AllowCredentials()
+                           .SetIsOriginAllowed(origin => true);
+                    });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, ILogger<Startup> logger)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors("CorsPolicy");
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            SeedDatabase(app);
+        }
+
+        void SeedDatabase(IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+                dbInitializer.Initialize();
+            }
+        }
+    }
+}
